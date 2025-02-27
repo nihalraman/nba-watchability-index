5aeddf25ecaaae7eeb262043be832621b5047c84
nihalraman <43160723+nihalraman@users.noreply.github.com>
2025-02-26 21:17:10 -0500

    defined some data models

--- nba_watchability_index/data/models.py
+++ nba_watchability_index/data/models.py
@@ -1,3 +1,52 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
+
+
+class City(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Team(models.Model):
+    nickname = models.CharField(max_length=200)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+
+class Player(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    teams = models.ManyToManyField(Team, through="PlayerTeam", related_name="players")
+
+
+class PlayerTeam(models.Model):
+    """Through model to link players with teams and add additional information."""
+
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    # Optional, if the player leaves the team
+    end_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        # A player can only be in one team at a time for a given period
+        unique_together = ("player", "team")
+
+
+class Game(models.Model):
+    home_team_id = models.ForeignKey(
+        Team, on_delete=models.CASCADE, related_name="home"
+    )
+    away_team_id = models.ForeignKey(
+        Team, on_delete=models.CASCADE, related_name="away"
+    )
+
+
+class PlayerGameScore(models.Model):
+    """Number of points scored by a player in a given game"""
+
+    player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
+    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    num_points = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(200)]
+    )
